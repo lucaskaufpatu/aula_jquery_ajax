fda79ac7345758ad93f543117f8c863df75d401e
Lucas Vasconcelos <64870228+lucaskaufpatu@users.noreply.github.com>
2020-06-22 14:05:56 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+
+function consultaCep(){
+    $(".barra-progresso").show();
+    var cep = document.getElementById ("cep").value;
+    var url = "http://viacep.com.br/ws/" + cep + "/json/";
+    console.log(url);
+
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            $("#Titulo_cep").html("CEP " + response.cep);
+            $(".cep").show();
+            $(".barra-progresso").hide();
+            //$("#logradouro").html(response.logradouro);
+        }
+
+    })
+}
+
+
+$(function(){
+    $(".cep").hide();
+    $(".barra-progresso").hide();
+}); 
+
+
